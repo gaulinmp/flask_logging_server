f01be5ff62dcd8ae3c70ec9b33f2208ffaf5e5bf
Mac Gaulin <gaulinmp+git@gmail.com>
2015-07-01 18:03:30 -0500

    Added example WSGI script

---
+++ example_wsgi.py
@@ -0,0 +1,21 @@
+#!/home1/USERNAME/python/2.7/bin/python
+# ^^ This is a custom install of python, change it to reflect your server ^^
+
+import os
+import sys
+sys.path.insert(0, '/home1/USERNAME/projects/flask_logging_server/')
+os.environ['LOGGING_UPLOAD_KEY'] = 'whatever you want the &key=BLAH thing to be'
+os.environ['SECRET_KEY'] = 'make me super secret yo'
+
+from flup.server.fcgi import WSGIServer
+# Change this line to run new app
+from baseapp import create_app
+
+from config.default_server import ProdConfig as config
+
+config.APPLICATION_ROOT = '/projects/log/' # <-- set this to this file
+
+app = create_app(config)
+
+if __name__ == '__main__':
+    WSGIServer(app).run()
